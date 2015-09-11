de6e0f0899b963855d5163bb35e4972f11ede1fc
AnnieHoogendoorn <anniehoogendoorn@gmail.com>
2015-09-11 13:40:44 -0700

    Add jquery to scripts.js, update index.html file and add css styles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
-var findAndReplace = function(inputString, word) {
+// var findAndReplace = function(inputString, word, newWord) {
+//
+//   var inputArray = inputString.split(" ");
+//   var outputArray = []
+//
+//   for (var i = 0; i < inputArray.length; i++) {
+//     if (inputArray[i] === word) {
+//     outputArray = inputArray.replace([i], newWord);
+//     }
+//   }
+//
+// }
 
-  var inputArray = inputString.split(" ");
 
-  for (var i = 0; i < inputArray.length; i++) {
-    if (inputArray[i] === word) {
-      return inputArray[i];
-    }
-  }
 
+
+var findAndReplace = function(input_string, word, new_word) {
+
+  // var input_string;
+  var output_string = input_string.replace(word, new_word);
+
+  return output_string;
 }
+
+
+$(document).ready(function() {
+  $("form#user_input").submit(function(event) {
+    var input_string = $("input#input_string").val();
+    var word = $("input#word").val();
+    var new_word = $("input#new_word").val();
+    var result = findAndReplace(input_string, word, new_word);
+
+    $("#result").text(result);
+    $("#results").show();
+    event.preventDefault();
+    debugger;
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,11 @@
 describe('findAndReplace', function() {
 
-  it('will find a word in a string', function() {
-    expect(findAndReplace('hello world', 'hello')).to.equal('hello');
-  });
+  // it('will find a word in a string', function() {
+  //   expect(findAndReplace('hello world', 'hello')).to.equal('hello');
+  // });
 
   it('will find a word in a string and replace it with a new word', function() {
-    expect(findAndReplace('hello world', 'hello', 'hi')).to.equal('hi');
+    expect(findAndReplace('hello world', 'hello', 'hi')).to.equal('hi world');
   });
   debugger;
 });
